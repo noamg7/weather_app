529979569d7a7f3deaca36257969954541161434
Noam <noamg@cox.net>
2016-02-22 13:43:32 -0500

    weatherapp

--- public/js/app.js
+++ public/js/app.js
@@ -1,63 +1,87 @@
 var weatherData = {};
 $(document).ready(function(){
-  var baseUrl = 'https://api.forecast.io/forecast/';
-  var weatherData = {};
+  var baseUrl = 'https://api.http://forecast.io/forecast/';
+  var locationData = {};
   $('#weather_app').on('click', showInfo);
 
   function buildUrl(city, state){
-    //return 'https://api.forecast.io/forecast/b6e3a9d6cbc211074ff9e60dc7bd71ec/37.8267,-122.423';
     return baseUrl + apiKey+'/'+city+','+state;
   }
 
-    function getWeather(){
-      var city = $('#city');
-      var state = $('#state');
-      var options = {
-        url: buildUrl(city, state),
-        dataType: 'jsonp',
-        success: successHandler,
-        error: errorHandler
-     };
-
-      $.ajax(options);
-    }
-
-    function successHandler(data){
-      weatherData = data;
-      $('#output').text(JSON.stringify(data));
-      console.log(data);
-      console.log(weatherData);
-    }
-
-    function errorHandler(err){
-      console.log(err);
-    }
-
-  function showInfo(){
+  function getLocationData(){
     var city = $('#city');
-    var state = $('#state');
+    var state= $('#state');
+    var options = {
+    url: buildUrl(city, state),
+    dataType: 'jsonp',
+    success: successHandler,
+    error: errorHandler,
+    };
+    $.ajax(options);
+  }
+  function buildUrl(lat, lon){
+    //return 'https://api.forecast.io/forecast/b6e3a9d6cbc211074ff9e60dc7bd71ec/37.8267,-122.423';
+    return baseUrl + apiKey+'/'+lat+','+lon;
+  }
+  function getWeather(){
+    var lat = $('#latitude').val();
+    var lon = $('#longitude').val();
+    var options = {
+      url: buildUrl(lat, lon),
+      dataType: 'jsonp',
+      success: successHandler,
+      error: errorHandler
+    };
+
+    $.ajax(options);
+  }
+
+  function successHandler(data){
+    weatherData = data;
+    locationData= data;
+    $('#output').text(JSON.stringify(data));
+    console.log(data);
+    console.log(locationData);
+    console.log(weatherData);
+  }
+
+  function errorHandler(err){
+    console.log(err);
+  }
+  function showInfo(data){
+    var lat = $('#latitude').val();
+    var lon = $('#longitude').val();
     var ajaxOptions = {
-      url: buildUrl(city, state),
+      url: buildUrl(lat, lon),
       dataType: 'jsonp',
       success: showInfoSuccess,
       error: errorHandler,
     };
     $.ajax(ajaxOptions);
   }
-
+  function showInfo(locationData){
+    var city = $('#city');
+    var state = $('#state');
+    var ajaxOptions = {
+      url: buildUrl(city, state),
+      dataType: 'jsnop',
+      success: showInfoSuccess,
+      error: errorHandler,
+    };
+    $.ajax(ajaxOptions);
+  }
   function showInfoSuccess(data){
     console.log(data);
     var source = $('#info').html();
     var template = Handlebars.compile(source);
     var extractedData = {
-      city: data.city,
-      state: data.state,
-      latitude: data.latitude.val(),
-      longitude: data.longitude.val(),
-      time: data.currently.time,
+     city: locationData.currently.city,
+     state: loationData.currently.state
+     latitude: data.latitude,
+     longitude: data.longitude,
+     time: data.currently.time,
     };
     var html = template(extractedData);
     $('#test-output').html(html);
   }
-
 });
